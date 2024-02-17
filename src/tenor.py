c46cfee450c5e4e52c69cfc0629fe159bf5f4379
Rougert Brian Alexandre <rougert.brian@gmail.com>
2024-02-17 09:48:18 -0300

    Changes to Tenor's API to try better filters for test precision

--- src/tenor.py
+++ src/tenor.py
@@ -8,23 +8,24 @@ load_dotenv()
 
 # set the apikey and limit
 apikey = os.getenv("TENOR_API")  # click to set to your apikey
-lmt = 8
+lmt = 5
 ckey = os.getenv("CLIENT_KEY")  # set the client_key for the integration and use the same value for all API calls
 
 def get_gif_pokemon(term):
-	# get the top 8 GIFs for the search term
+	term = term + " Pokemon"
+	# get the top 5 GIFs for the search term
 	r = requests.get(f"https://tenor.googleapis.com/v2/search?q={term}&key={apikey}&client_key={ckey}&limit={lmt}"
-					f"&media_filter=minimal&contentfilter=medium&media_filter=gif")
+					f"&media_filter=gif")
 
 	if r.status_code == 200:
 		# load the GIFs using the urls for the smaller GIF sizes
-		top_8gifs = json.loads(r.content)
-		top_8gifs = choice(top_8gifs["results"])
+		top_5gifs = json.loads(r.content)
+		top_5gifs = choice(top_5gifs["results"])
 	else:
-		top_8gifs = None
+		top_5gifs = None
 		print(r.status_code)
 
-	return top_8gifs["media_formats"]["gif"]["url"]
+	return top_5gifs["media_formats"]["gif"]["url"]
 
 if __name__ == "__main__":
-	print(get_gif_pokemon("Charmander"))
+	print(get_gif_pokemon("Walrein"))
